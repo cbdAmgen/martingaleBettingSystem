6fa730602ee24a3948e07b60c53f95989338a89b
Davis <cdavis09@amgen.com>
2019-01-04 22:32:19 -0800

    Need to work on expected winnings

--- R/martingale2.R
+++ R/martingale2.R
@@ -1,9 +1,9 @@
 rm(list = ls())
-# cat("\014")
+cat("\014")
 
 library(tidyverse)
 
-totalHands <- 6    # 1 minute per hand. This is where you set up your bets/time restriction
+totalHands <- 30    # 1 minute per hand. This is where you set up your bets/time restriction
 bankroll <- 1000    # starting amount in pocket. This is where you set up your wealth restriction
 B <- 1              # beginning bet
 
@@ -18,63 +18,64 @@ maxNumLosses <- 6
 
 
 
-goBustProb <- matrix(NA, ncol = maxNumLosses, nrow = totalHands)
-expectedWinnings <- matrix(NA, ncol = maxNumLosses, nrow = totalHands)
+goBustProb <- matrix(0, ncol = maxNumLosses, nrow = totalHands)
+expectedWinnings <- matrix(0, ncol = maxNumLosses, nrow = totalHands)
 system.time(
-for(i in 1:totalHands){
-  print(i)
-  blah <- matrix(NA, ncol = maxNumLosses, nrow = nSims)
-  # winnings <- matrix(NA, ncol = maxNumLosses, nrow = nSims)
-  for(j in 1:nSims){
-    
-    seqWinLoss <- rbinom(i, 1, p)         # simulate a sequence of wins and losses 
-    seqLengthAndValues <- rle(seqWinLoss) # The rle() function gives the length of
-                                          # all sequences and the value (Win or Loss)
-                                          # of each sequence
-    
-    allStreaks <- data.frame(length = seqLengthAndValues$lengths,
-                             value = seqLengthAndValues$values)
-    
-    streaksOfLosses <- allStreaks %>% 
-      filter(value == 0) # the sequences that were all losses
-    
-    numAtLeast <- sapply(1:maxNumLosses, function(x) sum(streaksOfLosses$length >= x))
+  for(i in 1:totalHands){
+    print(i)
+    blah <- matrix(0, ncol = maxNumLosses, nrow = nSims)
+    # winnings <- matrix(NA, ncol = maxNumLosses, nrow = nSims)
+    for(j in 1:nSims){
+      
+      seqWinLoss <- rbinom(i, 1, p)         # simulate a sequence of wins and losses 
+      seqLengthAndValues <- rle(seqWinLoss) # The rle() function gives the length of
+                                            # all sequences and the value (Win or Loss)
+                                            # of each sequence
+      
+      allStreaks <- data.frame(length = seqLengthAndValues$lengths,
+                               value = seqLengthAndValues$values)
+      
+      streaksOfLosses <- allStreaks %>% 
+        filter(value == 0) # the sequences that were all losses
+      
+      numAtLeast <- sapply(1:min(i, maxNumLosses), function(x) sum(streaksOfLosses$length >= x))
+      
+      
+      # for(k in 1:maxNumLosses){
+      #   winnings[j, k] <- sum(allStreaks$length*allStreaks$value)*(numAtLeast[k] == 0) +
+      #     -(2^k - 1)*(numAtLeast[k] > 0)
+      # }
+      
+      for(k in 1:maxNumLosses){
 
+        tmp <- 0
+        # tmpVec <- vector(mode = "numeric", length = maxNumLosses)
+        for(m in 1:nrow(allStreaks)){
+          if(allStreaks$length[m] == k && allStreaks$value[m] == 0){
+            tmp <- tmp - (2^k - 1)
+            # print(m)
+            break
+          }
+          else{
+            # print(m)
+            tmp <- tmp + allStreaks$length[m]*allStreaks$value[m]
+          }
+        }
+
+        winnings[j, k] <- tmp
+      }
+
+      blah[j, 1:length(numAtLeast)] <- numAtLeast
+      
+      
+    }
     
-    # for(k in 1:maxNumLosses){
-    #   winnings[j, k] <- sum(allStreaks$length*allStreaks$value)*(numAtLeast[k] == 0) +
-    #     -(2^k - 1)*(numAtLeast[k] > 0)
-    # }
-    
-    # for(k in 1:maxNumLosses){
-    #   
-    #   tmp <- 0
-    #   tmpVec <- vector(mode = "numeric", length = maxNumLosses)
-    #   for(m in 1:nrow(allStreaks)){
-    #     if(allStreaks$length[m] == k && allStreaks$value[m] == 0){
-    #       tmp <- tmp - (2^k - 1)
-    #       print(m)
-    #       break
-    #     } 
-    #     else{
-    #       print(m)
-    #       tmp <- tmp + allStreaks$length[m]*allStreaks$value[m]
-    #     }
-    #   }
-    #   
-    #   winnings[j, k] <- tmp
-    # }
-    # 
-    blah[j, ] <- numAtLeast
-    
+    blah2 <- (blah > 0)
+    goBustProb[i, ] <- colMeans(blah2)
+    # expectedWinnings[i, ] <- colMeans(winnings)
   }
-  
-  blah2 <- (blah > 0)
-  goBustProb[i, ] <- colMeans(blah2)
-  # expectedWinnings[i, ] <- colMeans(winnings)
-}
 )
-# goBustProb
+goBustProb
 # goBustProb <- round(goBustProb, 4)
 # expectedWinnings <- round(expectedWinnings, 2)
 # colnames(goBustProb) <- 1:ncol(goBustProb)
